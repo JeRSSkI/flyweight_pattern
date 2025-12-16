d19c5c76df391427a9e40baaf45e233f33894586
JeRSSkI <13553619@gsuite.duit.edu.ua>
2025-12-16 11:30:48 +0000

    Create tree_type.py

---
+++ tree_type.py
@@ -0,0 +1,8 @@
+class TreeType:
+    def __init__(self, name, color, texture):
+        self.name = name
+        self.color = color
+        self.texture = texture
+
+    def draw(self, x, y):
+        print(f"Drawing {self.name} tree of color {self.color} at ({x}, {y})")
